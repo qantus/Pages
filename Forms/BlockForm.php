879eaccf391397619f88b2eeb95c7c447aab1b44
Falaleev Maxim <max@studio107.ru>
2014-11-30 20:14:13 +0300

    Fix BlockForm

--- Forms/BlockForm.php
+++ Forms/BlockForm.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Pages\Forms;
 
-use Mindy\Form\Fields\TextField;
 use Mindy\Form\Fields\WysiwygField;
 use Mindy\Form\ModelForm;
 use Modules\Pages\Models\Block;
@@ -16,15 +15,15 @@ class BlockForm extends ModelForm
     public function getFields()
     {
         return [
-            'slug' => ['class' => TextField::className()],
-            'name' => ['class' => TextField::className()],
-            'content' => ['class' => WysiwygField::className()],
+            'content' => [
+                'class' => WysiwygField::className()
+            ],
         ];
 
     }
 
     public function getModel()
     {
-        return new Block();
+        return new Block;
     }
 }
